5f233c454fdfdc8624b04f286346c4390b864c1c
MatanP12 <matanp103@gmail.com>
2024-05-31 12:05:26 +0300

    Fix tests

--- e2e_tests/tests.sh
+++ e2e_tests/tests.sh
@@ -1,11 +1,11 @@
 
 # Build e2e tests image
-docker build --tag e2e_tests:latests ./e2e_tests
+docker build --tag e2e_tests ./e2e_tests
 # create a network and connect the proxy server
 docker network create test_network
 docker network connect test_network expense_app-proxy-1
 # run the test container
-docker run --network=test_network --rm --name e2e_test e2e_tests:latest
+docker run --network=test_network --rm --name e2e_test e2e_tests
 # get the exit code of docker run(0=tests passed other=tests failed)
 test_exit_code=$?
 # disconect the proxy server from the network and delete it
